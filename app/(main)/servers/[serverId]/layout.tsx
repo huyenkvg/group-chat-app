1b4dc472d8bc5a8450dc64f72d93d98335d8323e
huyenkvg <huyenkvg@gmail.com>
2024-01-06 02:08:13 +0700

    feat:  display member on Direct Message

--- app/(main)/servers/[serverId]/layout.tsx
+++ app/(main)/servers/[serverId]/layout.tsx
@@ -3,19 +3,50 @@ import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { currentProfile } from "@/lib/current-profile";
-import { Component, SendHorizontal } from "lucide-react";
-import { IChannel, IServer } from "@/typing/model-types";
+import { ChevronDown, Component, SendHorizontal } from "lucide-react";
+import { IChannel, IMember, IServer } from "@/typing/model-types";
 import { InviteModal } from "@/components/modals/invite-code/imvite-code-modal";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+const DMList = ({ members = [] }: { members: IMember[] }) => {
+  return (
+    <nav className="flex-1 px-2 py-4 bg-gray-800 space-y-1">
+      <a
+        href="#"
+        className=" text-white group flex items-center px-2 py-2 mb-2 tracking-wider text-base font-medium rounded-md gap-x-2 justify-between"
+      >
+        direct messages
+        <ChevronDown className="w-4 h-5 text-gray-200" />
+      </a>
+      {members.map((member: IMember) => {
+        return (
+          <a
+            key={member.id}
+            href={`/servers/${member.serverId}/members/${member.id}`}
+            className="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+          >
+            <span className="truncate flex flex-grow gap-2 items-center">
+              <Avatar>
+                <AvatarImage src={member.profile.imageUrl} />
+                <AvatarFallback>{member.profile.name}</AvatarFallback>
+              </Avatar>
+              {member.profile.name}
+            </span>
+          </a>
+        );
+      })}
+    </nav>
+  );
+};
 const ChannelList = ({ channels = [] }: { channels: IChannel[] }) => {
   return (
     <nav className="flex-1 px-2 py-4 bg-gray-800 space-y-1">
       <a
         href="#"
-        className="bg-gray-900 text-white group flex items-center px-2 py-2 mb-2 tracking-widest text-base font-medium rounded-md gap-x-2"
+        className=" text-white group flex items-center px-2 py-2 mb-2 tracking-wider text-base font-medium rounded-md gap-x-2 justify-between"
       >
-        <Component className="w-4 h-5 text-gray-200" />
         channels
+        <ChevronDown className="w-4 h-5 text-gray-200" />
       </a>
       {channels.map((channel: IChannel) => {
         const isDefaultChannel = channel.name === "general";
@@ -63,7 +94,11 @@ const ServerIdLayout = async ({
     },
     include: {
       channels: true,
-      members: true,
+      members: {
+        include: {
+          profile: true,
+        },
+      },
     },
   });
 
@@ -75,20 +110,15 @@ const ServerIdLayout = async ({
   return (
     <div className="h-full">
       <div className="flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        <div className="flex-1 flex flex-col overflow-y-auto">
-          <div className="flex-1 flex flex-col">
-            <ChannelList channels={server.channels as IChannel[]} />
-          </div>
-          {/* member list avatar ... */}
+        <div className="flex-1 flex flex-col overflow-y-auto divide-y divide-slate-900 ">
+          <ChannelList channels={server.channels as IChannel[]} />
+          <DMList members={server.members as IMember[]} />
           <div className="flex-shrink-0 flex bg-gray-700 p-2">
             <div className="flex items-center justify-between w-full">
               <span className="text-gray-300 text-sm">
                 Members: {server.members.length}
               </span>
-              <InviteModal
-                server={server as IServer}
-           
-              />
+              <InviteModal server={server as IServer} />
             </div>
           </div>
         </div>
