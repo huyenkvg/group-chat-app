4e7ca333e89b27cba72a4d686923bcf5eb0e2657
huyenkvg <huyenkvg@gmail.com>
2024-01-07 00:34:35 +0700

    feat: create chat-list componenent

---
+++ components/chat-components/chat-list.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { Fragment, useRef, ElementRef } from "react";
+import { Member, Message, Profile } from "@prisma/client";
+import { Loader2, ServerCrash } from "lucide-react";
+
+import { useChatQuery } from "@/hooks/use-chat-query";
+import dayjs from "dayjs";
+import { Avatar, AvatarImage } from "../ui/avatar";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
+
+type MessageWithMemberWithProfile = Message & {
+  member: Member & {
+    profile: Profile;
+  };
+};
+
+interface ChatListProps {
+  name: string;
+  member: Member;
+  chatId: string;
+  socketQuery: Record<string, string>;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+  isDirectMessage?: boolean;
+}
+
+export const ChatList = ({
+  name,
+  chatId,
+  paramKey,
+  paramValue,
+  isDirectMessage = false,
+}: ChatListProps) => {
+  const apiUrl = isDirectMessage ? "/api/direct-messages" : "/api/messages";
+
+  const queryKey = `chat:${chatId}`;
+
+  const chatRef = useRef<ElementRef<"div">>(null);
+  const bottomRef = useRef<ElementRef<"div">>(null);
+  const {
+    data,
+    fetchOlderMessages,
+    hasPreviousMessages,
+    isFetchingOlderMessages,
+    status,
+  } = useChatQuery({
+    queryKey,
+    apiUrl,
+    paramKey,
+    paramValue,
+  });
+
+  if (status === "error") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <ServerCrash className="h-7 w-7 text-zinc-500 my-4" />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Something went wrong!
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
+      {!hasPreviousMessages && <div className="flex-1" />}
+      {!hasPreviousMessages && (
+        <div className="flex justify-center">
+          <p className="text-xs text-zinc-500 dark:text-zinc-400">
+            {`--- Start of ${name} ---`}
+          </p>
+        </div>
+      )}
+      {hasPreviousMessages && (
+        <div className="flex justify-center">
+          {isFetchingOlderMessages ? (
+            <Loader2 className="h-6 w-6 text-zinc-500 animate-spin my-4" />
+          ) : (
+            <button
+              onClick={() => fetchOlderMessages()}
+              className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 text-xs my-4 dark:hover:text-zinc-300 transition"
+            >
+              Load more older messages
+            </button>
+          )}
+        </div>
+      )}
+      <div className="flex flex-col-reverse mt-auto">
+        {data?.pages?.map((group: any, index: number) => (
+          <Fragment key={index}>
+            {group.items.map((message: MessageWithMemberWithProfile) => (
+              <div key={message.id} className="flex flex-row">
+                <Avatar>
+                  <AvatarImage src={message.member.profile.imageUrl} />
+                </Avatar>
+                <div className="flex flex-col">
+                  <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    {dayjs(message.createdAt).format(DATE_FORMAT)}
+                  </p>
+                  <p>{message.member.profile.name}</p>
+                  <p>{message.content}</p>
+                </div>
+              </div>
+            ))}
+          </Fragment>
+        ))}
+      </div>
+      <div ref={bottomRef} />
+    </div>
+  );
+};
