4a3cbed070e309cb1a153a14f4341e8dfe90fe1c
huyenkvg <huyenkvg@gmail.com>
2024-01-07 19:22:27 +0700

    feat: create uoload file message modal

--- components/chat-components/message-input.tsx
+++ components/chat-components/message-input.tsx
@@ -2,7 +2,7 @@
 
 import * as z from "zod";
 // import qs from "query-string";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Paperclip, SendHorizontal } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
@@ -11,10 +11,11 @@ import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { useEffect, useState } from "react";
-import queryString from "query-string";
+import queryString, { StringifiableRecord } from "query-string";
+import ChatUploadFile from "../modals/chat-upload-modal/chat-upload-file";
 
 interface MessageInputProps {
-  query: Record<string, any>;
+  query: StringifiableRecord;
   name: string;
 }
 
@@ -24,6 +25,7 @@ const formSchema = z.object({
 
 export const MessageInput = ({ query, name }: MessageInputProps) => {
   const [isMounted, setIsMounted] = useState(false);
+  const [openUploadDialog, setOpenUploadDialog] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
@@ -42,12 +44,13 @@ export const MessageInput = ({ query, name }: MessageInputProps) => {
 
   const isLoading = form.formState.isSubmitting;
 
+  const chatApiUrl = isDirectMessage
+    ? "/api/socket/direct-messages"
+    : "/api/socket/messages";
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const url = queryString.stringifyUrl({
-        url: isDirectMessage
-          ? "/api/socket/direct-messages"
-          : "/api/socket/messages",
+        url: chatApiUrl,
         query,
       });
 
@@ -70,46 +73,55 @@ export const MessageInput = ({ query, name }: MessageInputProps) => {
     return null;
   }
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name="content"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <div className="relative p-2">
-                  <div className="rounded-t-lg bg-zinc-200/90 py-2 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0  text-zinc-600 dark:text-zinc-200">
-                    <Textarea
-                      disabled={isLoading}
-                      className="px-4 py-2 w-full bg-transparent border-none text-black dark:text-zinc-200"
-                      placeholder={`Message to ${
-                        isDirectMessage ? "@" + name : "#" + name
-                      }`}
-                      {...field}
-                    />
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div className="relative p-2">
+                    <div className="rounded-t-lg bg-zinc-200/90 py-2 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0  text-zinc-600 dark:text-zinc-200">
+                      <Textarea
+                        disabled={isLoading}
+                        className="px-4 py-2 w-full bg-transparent border-none text-black dark:text-zinc-200"
+                        placeholder={`Message to ${
+                          isDirectMessage ? "@" + name : "#" + name
+                        }`}
+                        {...field}
+                      />
+                    </div>
+                    <div className="bg-zinc-200/90 justify-between px-4 flex flex-row pb-2 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0  text-zinc-600 dark:text-zinc-200 rounded-b-lg">
+                      <Button
+                        type="button"
+                        onClick={() => setOpenUploadDialog(true)}
+                        className=" h-6 w-6 bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                      >
+                        <Paperclip className="text-white dark:text-[#313338]" />
+                      </Button>
+                      <Button
+                        type="submit"
+                        className=" h-[24px] px-4 bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                      >
+                        Send
+                        <SendHorizontal className="text-white dark:text-[#313338] p-1" />
+                      </Button>
+                    </div>
                   </div>
-                  <div className="bg-zinc-200/90 justify-between px-4 flex flex-row pb-2 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0  text-zinc-600 dark:text-zinc-200 rounded-b-lg">
-                    <Button
-                      type="button"
-                      className=" h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
-                    >
-                      <Paperclip className="text-white dark:text-[#313338]" />
-                    </Button>
-                    <Button
-                      type="submit"
-                      className=" h-[24px] px-4 bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
-                    >
-                      Send
-                      <SendHorizontal className="text-white dark:text-[#313338] p-1" />
-                    </Button>
-                  </div>
-                </div>
-              </FormControl>
-            </FormItem>
-          )}
-        />
-      </form>
-    </Form>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+      <ChatUploadFile
+        open={openUploadDialog}
+        onClose={() => setOpenUploadDialog(false)}
+        apiUrl={chatApiUrl}
+        query={query}
+      />
+    </div>
   );
 };
---
+++ components/modals/chat-upload-modal/chat-upload-file.tsx
@@ -0,0 +1,128 @@
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Paperclip } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import CustomModal from "../custom-modal";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { DialogFooter } from "@/components/ui/dialog";
+import { FileUpload } from "@/components/ui/file-upload";
+import qs, { StringifiableRecord } from "query-string";
+import { DialogProps } from "@radix-ui/react-dialog";
+import { RHFFormItem } from "@/components/RHF/RHFFormItem";
+import { InputField } from "@/components/RHF/RHFInputField";
+const formSchema = z.object({
+  fileUrl: z.string().min(1, {
+    message: "Attachment is required.",
+  }),
+  content: z.string().min(1, {
+    message: "Message is required.",
+  }),
+});
+
+const ChatUploadFile = ({
+  open,
+  apiUrl,
+  onClose,
+  query,
+}: {
+  open: boolean;
+  apiUrl: string;
+  query: StringifiableRecord;
+  onClose: () => void;
+}) => {
+  console.log('apiUrl', apiUrl)
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fileUrl: "",
+      content: "",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: apiUrl,
+        query,
+      });
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      if (!response.ok) {
+        throw new Error("failed to send file");
+      }
+      onClose();
+      form.reset();
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <CustomModal
+      title="Add an attachment"
+      description="Send a file"
+      open={open}
+      dialogProps={{
+        onOpenChange: onClose,
+      }}
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <div className="space-y-8 px-6">
+            <div className="flex items-center justify-center text-center">
+              <FormField
+                control={form.control}
+                name="fileUrl"
+                render={({ field }) => (
+                  <RHFFormItem description="uload a file">
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="messageFile"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  </RHFFormItem>
+                )}
+              />
+            </div>
+            <InputField
+              name="content"
+              formLabel="write a message"
+              inputProps={{
+                disabled: Boolean(
+                  form.formState.isSubmitting || !form.getValues("fileUrl")
+                ),
+              }}
+            />
+          </div>
+          <DialogFooter className="bg-gray-50 px-6 py-4 ">
+            <Button
+              variant="outline"
+              type="submit"
+              className="dark:text-gray-100 dark:border-none"
+              disabled={Boolean(form.formState.isSubmitting)}
+            >
+              SEND
+            </Button>
+          </DialogFooter>
+        </form>
+      </Form>
+    </CustomModal>
+  );
+};
+
+export default ChatUploadFile;
